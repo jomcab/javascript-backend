e4032e1d3e543f5474b12ca96293e9651df4b0f7
jomcab <jomercabrera@gmail.com>
2022-11-16 00:57:40 +0800

    Readable and Writable Stream
    Reading txt file line by line.
    Writing to txt file.

---
+++ 03 Readable and Writable Stream/Reading File Stream.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+const fs = require('fs');
+
+let myInterface = readline.createInterface({
+  input: fs.createReadStream('shoppingList.txt')
+});
+
+const printData = (data) => {
+  console.log(`Item: ${data}`);
+}
+
+myInterface.on('line', printData)
